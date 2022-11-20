96408babd59e369089c251a6b7ad125bf8799cf2
ferutizi <fer.utizi@gmail.com>
2022-11-20 17:11:49 -0300

    function previous

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ function App() {
   const next = () => {
     debugger;
     const condition = index < images.length -1 ? true : false;
-    if(condition) {
-      setIndex(index +1);
-    } else {
-      setIndex(0);
-    }
-    setCurrentImg(images[index])
+    condition ? setIndex(index +1) : setIndex(0);
+    setCurrentImg(images[index]);
+  }
+
+  const previous = () => {
+    debugger;
+    const condition = index > 0 ? true : false;
+    condition ? setIndex(index -1) : setIndex(images.length -1);
+    setCurrentImg(images[index]);
   }
 
 
@@ -22,6 +25,7 @@ function App() {
       <h1>Carousel</h1>
       <img src={require(`./images/${currentImg}`)} alt='cat' style={{maxWidth: '400px'}}></img>
       <button onClick={() => next()}>next</button>
+      <button onClick={() => previous()}>previous</button>
     </div>
   );
 }
