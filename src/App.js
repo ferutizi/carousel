5cf9de6451d53ddcfb5a68be98919cbcec82ad2d
ferutizi <fer.utizi@gmail.com>
2022-11-20 17:58:34 -0300

    style y responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import './App.css';
 
 function App() {
   const images = ['cat.jpg', 'cat_1.jpg', 'cat_2.jpg'];
@@ -6,14 +7,12 @@ function App() {
   const [currentImg, setCurrentImg] = useState(images[index]);
 
   const next = () => {
-    debugger;
     const condition = index < images.length -1 ? true : false;
     condition ? setIndex(index +1) : setIndex(0);
     setCurrentImg(images[index]);
   }
 
   const previous = () => {
-    debugger;
     const condition = index > 0 ? true : false;
     condition ? setIndex(index -1) : setIndex(images.length -1);
     setCurrentImg(images[index]);
@@ -21,12 +20,16 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>Carousel</h1>
-      <img src={require(`./images/${currentImg}`)} alt='cat' style={{maxWidth: '400px'}}></img>
-      <button onClick={() => next()}>next</button>
-      <button onClick={() => previous()}>previous</button>
-    </div>
+    <>
+      <h1>Cats</h1>
+      <div className="container">
+        <div className="img__container">
+          <img src={require(`./images/${currentImg}`)} alt='cat' style={{maxWidth: '600px'}}></img>
+        </div>
+        <button onClick={() => previous()}>previous</button>
+        <button onClick={() => next()}>next</button>
+      </div>
+    </>
   );
 }
 
