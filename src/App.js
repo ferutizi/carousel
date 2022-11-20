c2d65c8559918dc878e8c4684d5590da3f0de1b8
ferutizi <fer.utizi@gmail.com>
2022-11-20 17:03:21 -0300

    states y function next

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
 
 function App() {
+  const images = ['cat.jpg', 'cat_1.jpg', 'cat_2.jpg'];
+  const [index, setIndex] = useState(0);
+  const [currentImg, setCurrentImg] = useState(images[index]);
+
+  const next = () => {
+    debugger;
+    const condition = index < images.length -1 ? true : false;
+    if(condition) {
+      setIndex(index +1);
+    } else {
+      setIndex(0);
+    }
+    setCurrentImg(images[index])
+  }
+
+
   return (
     <div className="App">
-
+      <h1>Carousel</h1>
+      <img src={require(`./images/${currentImg}`)} alt='cat' style={{maxWidth: '400px'}}></img>
+      <button onClick={() => next()}>next</button>
     </div>
   );
 }
