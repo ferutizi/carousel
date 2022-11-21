ce6aca6a780a72225205f7724de2d3f5056b01bb
ferutizi <fer.utizi@gmail.com>
2022-11-20 22:02:03 -0300

    fix isNext

--- src/App.js
+++ src/App.js
@@ -6,13 +6,24 @@ function App() {
   const [index, setIndex] = useState(0);
   const [currentImg, setCurrentImg] = useState(images[index]);
 
+/*   const isNext = (next) => {
+    let newIndex = 0;
+    next ? 
+    newIndex = (index +1)  % images.length
+    : newIndex = (index + images.length -1)  % images.length;
+    setIndex(newIndex);
+    setCurrentImg(images[newIndex]);
+  } */
+
   const isNext = (next) => {
+    let newIndex = 0;
     const condition = index < images.length -1 ? true : false;
     const condition2 = index > 0 ? true : false;
     next ? 
-      condition ? setIndex(index +1) : setIndex(0)
-      : condition2 ? setIndex(index -1) : setIndex(images.length -1);
-    setCurrentImg(images[index]);
+      condition ? newIndex = index +1 : newIndex = 0
+      : condition2 ? newIndex = index -1 : newIndex = images.length -1;
+    setIndex(newIndex);
+    setCurrentImg(images[newIndex]);
   }
 
   return (
@@ -22,8 +33,8 @@ function App() {
         <div className="img__container">
           <img src={require(`./images/${currentImg}`)} alt='cat' style={{maxWidth: '600px'}}></img>
         </div>
-        <button onClick={() => isNext(true)}>Previous</button>
-        <button onClick={() => isNext(false)}>Next</button>
+        <button onClick={() => isNext(false)}>Previous</button>
+        <button onClick={() => isNext(true)}>Next</button>
       </div>
     </>
   );
